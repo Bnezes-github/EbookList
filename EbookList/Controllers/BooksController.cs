cb3d9698d462590a9eb556a0bab9a72bcc62c621
BRUNO ALMEIDA <bruno.nezes@gmail.com>
2020-01-22 21:57:50 +0000

    Add project files.

---
+++ EbookList/Controllers/BooksController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EbookList.Models;
+using EbookList.Data;
+
+namespace EbookList.Controllers
+{
+    public class BooksController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;    
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Books.ToList());
+        }
+
+        //GET book/Create
+
+      public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+    }
+}
